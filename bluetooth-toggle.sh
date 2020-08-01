63cbef375cab5b0b01caef3d3fd64301edc70b0e
aliefamzari <aliefamzari@gmail.com>
2020-08-01 14:50:44 +0800

    first init

--- bluetooth-toggle.sh
+++ bluetooth-toggle.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+sleep 1
+bstatus=$(rfkill |grep 'tpacpi_bluetooth_sw' |awk '{print $4}')
+
+function bt_start {
+	sudo systemctl start bluetooth > /dev/null 2>&1
+}
+
+function bt_off {
+	sudo systemctl stop bluetooth > /dev/null 2>&1
+}
+
+icon=
+color="#1AE300"
+
+if [ "$bt_status" = 'blocked' ]; then
+	bt_off
+    icon=
+	color="#969997"
+	echo "$icon Off\n"
+	echo $color
+else
+    bt_start
+	echo "$icon On\n"
+	echo $color
+fi
+#[ $bstatus = "blocked" ] && bt_off || bt_start
+
